2be57343e217294c04935c60366da152aebd911d
Bleik <91410898+Eligoswwww@users.noreply.github.com>
2025-05-17 18:15:16 +0000

    Update user.py

--- keyboards/user.py
+++ keyboards/user.py
@@ -1,7 +1,8 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-USER_PANEL = InlineKeyboardMarkup(row_width=1)
-USER_PANEL.add(
-    InlineKeyboardButton(text="Мои подписки и покупки", callback_data="user_my_access"),
-    InlineKeyboardButton(text="Купить подписку", callback_data="user_buy_subscription"),
+USER_PANEL = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="Кнопка 1", callback_data="button_1")],
+        [InlineKeyboardButton(text="Кнопка 2", callback_data="button_2")],
+    ]
 )
