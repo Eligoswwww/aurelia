e2ff551512d7fac4d1b5b54a16aa1416019c576f
Bleik <91410898+Eligoswwww@users.noreply.github.com>
2025-05-17 23:43:02 +0000

    Update user.py

--- keyboards/user.py
+++ keyboards/user.py
@@ -6,7 +6,9 @@ USER_PANEL = InlineKeyboardMarkup(
         [InlineKeyboardButton(text="Полный доступ", callback_data="full_access")],
         [InlineKeyboardButton(text="Посмотреть в магазине", url="https://a.co/d/0lsFYNT")],
         [InlineKeyboardButton(text="Подписаться", callback_data="subscribe")],
-        # keyboards/user.py
+    ]  # <-- закрываем список inline_keyboard
+)  # <-- закрываем InlineKeyboardMarkup
+
 FULL_ACCESS_PANEL = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton(text="Читать всю книгу", callback_data="read_full_book")],
