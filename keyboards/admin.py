538c50781f55b03c3856f77a6083d0bebc4a4914
Bleik <91410898+Eligoswwww@users.noreply.github.com>
2025-05-17 18:11:02 +0000

    Update admin.py

--- keyboards/admin.py
+++ keyboards/admin.py
@@ -1,8 +1,13 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-ADMIN_PANEL = InlineKeyboardMarkup(row_width=2)
-ADMIN_PANEL.add(
-    InlineKeyboardButton(text="Создать заказ подписки", callback_data="admin_create_subscription"),
-    InlineKeyboardButton(text="Создать заказ главы", callback_data="admin_create_chapter"),
-    InlineKeyboardButton(text="Проверить статус пользователя", callback_data="admin_check_user"),
+ADMIN_PANEL = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Создать заказ подписки", callback_data="admin_create_subscription"),
+            InlineKeyboardButton(text="Создать заказ главы", callback_data="admin_create_chapter"),
+        ],
+        [
+            InlineKeyboardButton(text="Проверить статус пользователя", callback_data="admin_check_user"),
+        ],
+    ]
 )
