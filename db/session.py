861be36f6c0d62416ff6f1af230b9567049ebf6d
Bleik <91410898+Eligoswwww@users.noreply.github.com>
2025-05-17 17:53:28 +0000

    Update session.py

--- db/session.py
+++ db/session.py
@@ -1,6 +1,10 @@
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from dotenv import load_dotenv
 
-DATABASE_URL = os.getenv("DATABASE_URL")  # Например: postgresql+asyncpg://user:pass@host/dbname
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
