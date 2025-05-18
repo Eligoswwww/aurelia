1fb0b6e535f3ca7d1e9a8c6cfa39d740f81ed28c
Bleik <91410898+Eligoswwww@users.noreply.github.com>
2025-05-18 03:01:10 +0000

    Update user.py

--- handlers/user.py
+++ handlers/user.py
@@ -1,9 +1,7 @@
-# handlers/admin.py
-
 from aiogram import types
-
-def register_handlers(dp):
-    dp.message_handler(commands=["admin"])(admin_command)
 
 async def admin_command(message: types.Message):
     await message.answer("Admin команда выполнена!")
+
+def register_handlers(dp):
+    dp.message.register(admin_command, commands=["admin"])
